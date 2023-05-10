ec56601314af9fcc93d3ef00869db94f8b26c85f
Liu Chengwu <liuchengwu@pku.edu.cn>
2023-05-10 10:44:40 +0800

    Update example.py

--- src/example.py
+++ src/example.py
@@ -1,14 +1,24 @@
 import encode
 import decode
 
+def process(text):
+    code = encode.encode(text)
+    print('encoded: ', code)
+    print('decoded: ', decode.decode(code))
+
 if __name__ == '__main__':
     text = '吾輩は猫である。名前はまだ無い。'
+    print('original: ', text)
 
     # code = encode.encode_list(text)
     # print(code)
     # print(decode.decode_list(code))
 
-    print('original: ', text)
-    code = encode.encode(text)
-    print('encoded: ', code)
-    print('decoded: ', decode.decode(code))
+    process(text)
+    
+    while True:
+        text = input('original: ')
+        if not text:
+            print('Goodbye!')
+            break
+        process(text)
